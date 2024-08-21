f3b0bc88f3c5666eb8afb8c9b6884fb1cf3e75e7
ttk <ttktatakai@outlook.com>
2024-08-21 16:40:34 +0800

    refactor

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,36 @@
 package main
 
-import "github.com/spf13/pflag"
+import (
+	"errors"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/oklog/run"
+	"github.com/veops/oneterm/api"
+	"github.com/veops/oneterm/logger"
+	"go.uber.org/zap"
+)
 
 func main() {
-	path := pflag.StringP("config", "c", "config.yaml", "config path")
-	
+	rg := run.Group{}
+	{
+		term := make(chan os.Signal, 1)
+		signal.Notify(term, os.Interrupt, syscall.SIGTERM)
+		rg.Add(func() error {
+			<-term
+			return errors.New("terminated")
+		}, func(err error) {})
+	}
+	{
+		rg.Add(func() error {
+			return api.RunApi()
+		}, func(err error) {
+			api.StopApi()
+		})
+	}
+
+	if err := rg.Run(); err != nil {
+		logger.L().Fatal("", zap.Error(err))
+	}
 }

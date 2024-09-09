1c5a78de6fb6dac96d29eb5fd7a811653fa4148f
songlh <song.lhlh@gmail.com>
2024-09-09 14:48:12 +0800

    feat(ui): update dashboard userRank ui

--- oneterm-ui/src/modules/oneterm/views/dashboard/userRank.vue
+++ oneterm-ui/src/modules/oneterm/views/dashboard/userRank.vue
@@ -5,6 +5,7 @@
       <template v-for="(item, index) in rankList">
         <div class="user-rank-box" :key="item.uid">
           <a-avatar
+            class="user-rank-box-avatar"
             v-if="getUserKeyByUid(item.uid, 'avatar')"
             :size="36"
             :src="
@@ -13,11 +14,18 @@
                 : `/api/common-setting/v1/file/${getUserKeyByUid(item.uid, 'avatar')}`
             "
           />
-          <a-avatar v-else :style="{ backgroundColor: '#2F54EB', fontSize: '12px' }" :size="36">
+          <a-avatar
+            v-else
+            class="user-rank-box-avatar"
+            :style="{ backgroundColor: '#2F54EB', fontSize: '12px' }"
+            :size="36"
+          >
             {{ getUserKeyByUid(item.uid).substring(0, 1) }}
           </a-avatar>
-          <div>
-            <div class="user-rank-box-name">{{ getUserKeyByUid(item.uid) }}</div>
+          <div class="user-rank-box-center">
+            <a-tooltip :title="getUserKeyByUid(item.uid)" placement="topLeft" >
+              <div class="user-rank-box-name">{{ getUserKeyByUid(item.uid) }}</div>
+            </a-tooltip>
             <div class="user-rank-box-time">{{ moment(item.last_time).format('YYYY-MM-DD HH:mm:ss') }}</div>
           </div>
           <div class="user-rank-box-count">
@@ -76,24 +84,44 @@ export default {
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: space-between;
     padding: 10px 0;
     position: relative;
+
+    &-avatar {
+      flex-shrink: 0;
+    }
+
+    &-center {
+      margin-left: 16px;
+      margin-right: 16px;
+      max-width: 100%;
+      overflow: hidden;
+    }
+
     .user-rank-box-name {
       color: #252631;
       font-weight: 400;
       font-size: 14px;
+      overflow: hidden;
+      text-wrap: nowrap;
+      text-overflow: ellipsis;
+      max-width: 100%;
     }
     .user-rank-box-time {
       color: #98a9bc;
       font-weight: 400;
       font-size: 14px;
     }
-    .user-rank-box-count > span {
-      color: @primary-color;
-      background-color: #dce3fb;
-      padding: 0 10px;
-      border-radius: 1px;
+    .user-rank-box-count {
+      margin-left: auto;
+      flex-shrink: 0;
+
+      & > span {
+        color: @primary-color;
+        background-color: #dce3fb;
+        padding: 0 10px;
+        border-radius: 1px;
+      }
     }
   }
 }
